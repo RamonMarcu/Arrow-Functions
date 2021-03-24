dc33ff38824416a70c8541a4b5d24185df508e1a
Ramon Marcu <ramondotmarcu@gmail.com>
2021-03-24 15:16:06 +0200

    Add files via upload

---
+++ Arrow Functions.js
@@ -0,0 +1,16 @@
+//main.js
+
+const plantNeedsWater = (day) => {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(plantNeedsWater('Wednesday'));
+
+//if run, console displays:
+
+true
+
